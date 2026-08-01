1cf870aba87323d3f017558360407c168af1b7af
codebyradha <rradhad588@gmail.com>
2026-08-01 10:04:02 +0530

    a

--- caluculator.js
+++ caluculator.js
@@ -9,3 +9,9 @@ function calculator(a,b)
     console.log("Division:",a ,"and ",b,"is:",a/b);
 }
 calculator(10,5);
+
+function calculation(a,b)
+{
+    console.log("multiplication:",a+b*b);
+    console.log("addtion:",a+b+b+a);
+}
